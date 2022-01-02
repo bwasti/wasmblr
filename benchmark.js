d6ad335adab487c6c54203da747b37d9a90acc81
Bram Wasti <bwasti@brams-air.myfiosgateway.com>
2022-01-02 14:11:22 -0500

    add no simd variant

--- benchmark.js
+++ benchmark.js
@@ -3,200 +3,200 @@ const target_ms = 50;
 const tuning_ms = 10;
 
 function log(...args) {
-  const str = args.reduce((a, b) => {
-    return a + " " + b;
-  }, "");
-  document.querySelector('pre').appendChild(document.createTextNode(str));
-  document.querySelector('pre').appendChild(document.createElement('br'));
+    const str = args.reduce((a, b) => {
+        return a + " " + b;
+    }, "");
+    document.querySelector('pre').appendChild(document.createTextNode(str));
+    document.querySelector('pre').appendChild(document.createElement('br'));
 }
 
 async function gen_pure(N) {
-  let a = new Array(N).fill(0);
-  let b = new Array(N).fill(0);
-  let c = new Array(N).fill(0);
+    let a = new Array(N).fill(0);
+    let b = new Array(N).fill(0);
+    let c = new Array(N).fill(0);
 
-  function add() {
-    for (let i = 0; i < N; ++i) {
-      c[i] = a[i] + b[i];
+    function add() {
+        for (let i = 0; i < N; ++i) {
+            c[i] = a[i] + b[i];
+        }
     }
-  }
 
-  return [add, a, b, c];
+    return [add, a, b, c];
 }
 
 async function gen_typed(N) {
-  let a = new Float32Array(N);
-  let b = new Float32Array(N);
-  let c = new Float32Array(N);
+    let a = new Float32Array(N).fill(0);
+    let b = new Float32Array(N).fill(0);
+    let c = new Float32Array(N).fill(0);
 
-  function add() {
-    for (let i = 0; i < N; ++i) {
-      c[i] = a[i] + b[i];
+    function add() {
+        for (let i = 0; i < N; ++i) {
+            c[i] = a[i] + b[i];
+        }
     }
-  }
 
-  return [add, a, b, c];
+    return [add, a, b, c];
 }
 
 async function gen_emscripten(Module, N) {
-  function emscripten_array(len) {
-    var ptr = Module._malloc(len * 4);
-    return [new Float32Array(Module.HEAPF32.buffer, ptr, len), ptr];
-  }
+    function emscripten_array(len) {
+        var ptr = Module._malloc(len * 4);
+        return [new Float32Array(Module.HEAPF32.buffer, ptr, len), ptr];
+    }
 
-  let [a, a_] = emscripten_array(N);
-  let [b, b_] = emscripten_array(N);
-  let [c, c_] = emscripten_array(N);
-  const add = Module._add;
+    let [a, a_] = emscripten_array(N);
+    let [b, b_] = emscripten_array(N);
+    let [c, c_] = emscripten_array(N);
+    const add = Module._add;
 
-  return [() => add(a_, b_, c_, N), a, b, c, () => {
-    Module._free(a_);
-    Module._free(b_);
-    Module._free(c_);
-  }];
+    return [() => add(a_, b_, c_, N), a, b, c, () => {
+        Module._free(a_);
+        Module._free(b_);
+        Module._free(c_);
+    }];
 }
 
 async function gen_wasmblr(Module, N, unroll) {
-  const wasm = Module._jit_add(N, unroll);
-  const wasm_len = Module._jit_add_len(N, unroll);
-  const wasm_data = new Uint8Array(Module.HEAP8.buffer, wasm, wasm_len);
-  const m = await WebAssembly.compile(wasm_data);
-  const instance = await WebAssembly.instantiate(m, {});
-  Module._free(wasm);
+    const wasm = Module._jit_add(N, unroll);
+    const wasm_len = Module._jit_add_len(N, unroll);
+    const wasm_data = new Uint8Array(Module.HEAP8.buffer, wasm, wasm_len);
+    const m = await WebAssembly.compile(wasm_data);
+    const instance = await WebAssembly.instantiate(m, {});
+    Module._free(wasm);
 
-  let wasmblr_malloc_height = 0;
-  let mem = instance.exports.mem;
+    let wasmblr_malloc_height = 0;
+    let mem = instance.exports.mem;
 
-  function wasmblr_array(len) {
-    console.assert((mem.buffer.byteLength - wasmblr_malloc_height) > len * 4);
-    let ptr = wasmblr_malloc_height;
-    let array = new Float32Array(mem.buffer, ptr, len);
-    wasmblr_malloc_height += len * 4;
-    return [array, ptr];
-  }
-  let [a, a_] = wasmblr_array(N);
-  let [b, b_] = wasmblr_array(N);
-  let [c, c_] = wasmblr_array(N);
+    function wasmblr_array(len) {
+        console.assert((mem.buffer.byteLength - wasmblr_malloc_height) > len * 4);
+        let ptr = wasmblr_malloc_height;
+        let array = new Float32Array(mem.buffer, ptr, len);
+        wasmblr_malloc_height += len * 4;
+        return [array, ptr];
+    }
+    let [a, a_] = wasmblr_array(N);
+    let [b, b_] = wasmblr_array(N);
+    let [c, c_] = wasmblr_array(N);
 
-  const add = instance.exports.add;
+    const add = instance.exports.add;
 
-  return [() => add(a_, b_, c_), a, b, c, ()=>{}];
+    return [() => add(a_, b_, c_), a, b, c, () => {}];
 }
 
 async function gen_wasmblr_tuned(Module, N) {
-  let best = 0;
-  let best_time = 0;
-  for (let i = 0; Math.pow(2, i) < Math.min(1024, N / 4 + 2); ++i) {
-    let [fn, w_a, w_b, w_c, cleanup] = await gen_wasmblr(Module, N, Math.pow(2, i));
+    let best = 0;
+    let best_time = 0;
+    for (let i = 0; Math.pow(2, i) < Math.min(1024, N / 4 + 2); ++i) {
+        let [fn, w_a, w_b, w_c, cleanup] = await gen_wasmblr(Module, N, Math.pow(2, i));
 
-    let diff = 0;
-    let num_iters = 10;
-    while (diff < tuning_ms) {
-      num_iters *= 2;
-      const t0 = performance.now();
-      for (let i = 0; i < num_iters; ++i) {
-        fn();
-      }
-      const t1 = performance.now();
-      diff = t1 - t0;
-    }
+        let diff = 0;
+        let num_iters = 10;
+        while (diff < tuning_ms) {
+            num_iters *= 2;
+            const t0 = performance.now();
+            for (let i = 0; i < num_iters; ++i) {
+                fn();
+            }
+            const t1 = performance.now();
+            diff = t1 - t0;
+        }
 
-    if ((num_iters / diff) > best_time) {
-      best = i;
-      best_time = num_iters / diff;
+        if ((num_iters / diff) > best_time) {
+            best = i;
+            best_time = num_iters / diff;
+        }
+        cleanup();
     }
-    cleanup();
-  }
-  return [...await gen_wasmblr(Module, N, Math.pow(2, best)), Math.pow(2, best)];
+    return [...await gen_wasmblr(Module, N, Math.pow(2, best)), Math.pow(2, best)];
 }
 
 function perf(N, name, fn) {
-  let diff = 0;
-  let num_iters = 10;
-  fn(); // warmup with 1 run
-  while (diff < (target_ms / 2)) {
-    num_iters *= 2;
-    const w0 = performance.now();
-    for (let i = 0; i < num_iters; ++i) {
-      fn();
+    let diff = 0;
+    let num_iters = 10;
+    fn(); // warmup with 1 run
+    while (diff < (target_ms / 2)) {
+        num_iters *= 2;
+        const w0 = performance.now();
+        for (let i = 0; i < num_iters; ++i) {
+            fn();
+        }
+        const w1 = performance.now();
+        diff = w1 - w0;
     }
-    const w1 = performance.now();
-    diff = w1 - w0;
-  }
-  const iters_sec = 1e3 * num_iters / diff;
-  const elem_sec = N * iters_sec;
-  const gb_sec = elem_sec * 4 * 3 /* 2 read 1 write */ / 1e9;
-  const round = (num) => Math.round(num * 100) / 100
-  log(name, `${round(gb_sec)} GB/s`, `(${round(iters_sec)} iters/sec)`);
-  return gb_sec;
+    const iters_sec = 1e3 * num_iters / diff;
+    const elem_sec = N * iters_sec;
+    const gb_sec = elem_sec * 4 * 3 /* 2 read 1 write */ / 1e9;
+    const round = (num) => Math.round(num * 100) / 100
+    log(name, `${round(gb_sec)} GB/s`, `(${round(iters_sec)} iters/sec)`);
+    return gb_sec;
 }
 
 async function benchmark(Module, N) {
-  let [pure_fn, p_a, p_b, p_c] = await gen_pure(N);
-  let [typed_fn, t_a, t_b, t_c] = await gen_typed(N);
-  let [emscripten_fn, e_a, e_b, e_c, emscripten_cleanup] = await gen_emscripten(Module, N);
-  let [wasmblr_fn, w_a, w_b, w_c, wasmblr_cleanup] = await gen_wasmblr(Module, N, wasmblr_unroll);
-  let [wasmblr_tuned_fn, wt_a, wt_b, wt_c, wasmblr_tuned_cleanup, unroll] = await gen_wasmblr_tuned(Module, N);
+    let [pure_fn, p_a, p_b, p_c] = await gen_pure(N);
+    let [typed_fn, t_a, t_b, t_c] = await gen_typed(N);
+    let [emscripten_fn, e_a, e_b, e_c, emscripten_cleanup] = await gen_emscripten(Module, N);
+    let [wasmblr_fn, w_a, w_b, w_c, wasmblr_cleanup] = await gen_wasmblr(Module, N, wasmblr_unroll);
+    let [wasmblr_tuned_fn, wt_a, wt_b, wt_c, wasmblr_tuned_cleanup, unroll] = await gen_wasmblr_tuned(Module, N);
 
-  for (let i = 0; i < N; ++i) {
-    let a = Math.random();
-    let b = Math.random();
-    p_a[i] = a;
-    t_a[i] = a;
-    e_a[i] = a;
-    w_a[i] = a;
-    wt_a[i] = a;
+    for (let i = 0; i < N; ++i) {
+        let a = Math.random();
+        let b = Math.random();
+        p_a[i] = a;
+        t_a[i] = a;
+        e_a[i] = a;
+        w_a[i] = a;
+        wt_a[i] = a;
 
-    p_b[i] = b;
-    t_b[i] = b;
-    e_b[i] = b;
-    w_b[i] = b;
-    wt_b[i] = b;
-  }
-
-  pure_fn();
-  typed_fn();
-  emscripten_fn();
-  wasmblr_fn();
-  wasmblr_tuned_fn();
-
-  for (let i = 0; i < N; ++i) {
-    function check(arr, name) {
-      if (Math.abs(t_c[i] - arr[i]) > 0.01) {
-        console.log("difference found at index", i, t_c[i], "vs", name, arr[i]);
-        return false;
-      }
-      return true;
+        p_b[i] = b;
+        t_b[i] = b;
+        e_b[i] = b;
+        w_b[i] = b;
+        wt_b[i] = b;
     }
-    if (!check(p_c, "pure")) {
-      console.log("error");
-      return;
-    }
-    if (!check(e_c, "emscripten")) {
-      console.log("error");
-      return;
-    }
-    if (!check(w_c, "wasmblr")) {
-      console.log("error");
-      return;
-    }
-    if (!check(wt_c, "wasmblr (tuned)")) {
-      console.log("error");
-      return;
-    }
-  }
 
-  log();
-  log("benchmarking vec add of size", N);
-  const p_gbs = perf(N, "  pure javascript:        ", pure_fn);
-  const t_gbs = perf(N, "  typed arrays:           ", typed_fn);
-  const e_gbs = perf(N, "  emscripten (simd):      ", emscripten_fn);
-  const w_gbs = perf(N, "  wasmblr:                ", wasmblr_fn);
-  const wt_gbs = perf(N, `  wasmblr (tuned ${unroll}):`.padEnd(26), wasmblr_tuned_fn);
+    pure_fn();
+    typed_fn();
+    emscripten_fn();
+    wasmblr_fn();
+    wasmblr_tuned_fn();
 
-  emscripten_cleanup()
-  wasmblr_cleanup()
-  wasmblr_tuned_cleanup()
+    for (let i = 0; i < N; ++i) {
+        function check(arr, name) {
+            if (Math.abs(t_c[i] - arr[i]) > 0.01) {
+                console.log("difference found at index", i, t_c[i], "vs", name, arr[i]);
+                return false;
+            }
+            return true;
+        }
+        if (!check(p_c, "pure")) {
+            console.log("error");
+            return;
+        }
+        if (!check(e_c, "emscripten")) {
+            console.log("error");
+            return;
+        }
+        if (!check(w_c, "wasmblr")) {
+            console.log("error");
+            return;
+        }
+        if (!check(wt_c, "wasmblr (tuned)")) {
+            console.log("error");
+            return;
+        }
+    }
 
-  return [p_gbs, t_gbs, e_gbs, w_gbs, wt_gbs];
+    log();
+    log("benchmarking vec add of size", N);
+    const p_gbs = perf(N, "  pure javascript:        ", pure_fn);
+    const t_gbs = perf(N, "  typed arrays:           ", typed_fn);
+    const e_gbs = perf(N, "  emscripten (simd):      ", emscripten_fn);
+    const w_gbs = perf(N, "  wasmblr:                ", wasmblr_fn);
+    const wt_gbs = perf(N, `  wasmblr (tuned ${unroll}):`.padEnd(26), wasmblr_tuned_fn);
+
+    emscripten_cleanup()
+    wasmblr_cleanup()
+    wasmblr_tuned_cleanup()
+
+    return [p_gbs, t_gbs, e_gbs, w_gbs, wt_gbs];
 }
---
+++ init.js
@@ -0,0 +1,186 @@
+      function remove(elem) {
+          while (elem.firstChild) {
+              elem.removeChild(elem.firstChild);
+          }
+      }
+
+      function error() {
+          const out = document.querySelector("#graph");
+          let b = document.createElement('b');
+          b.appendChild(document.createTextNode("Cannot run SIMD on this browser.  Please try Chrome or Firefox (or refresh to try again)"));
+          out.appendChild(b);
+          setTimeout(function() {
+              out.appendChild(document.createElement('br'));
+              out.appendChild(document.createElement('br'));
+              out.appendChild(document.createTextNode("To run in node.js, please grab the code here:"));
+              out.appendChild(document.createElement('br'));
+              let a = document.createElement('a');
+              a.href = "https://github.com/bwasti/wasmblr/tree/main/emscripten_example";
+              a.textContent = "https://github.com/bwasti/wasmblr/tree/main/emscripten_example";
+              out.appendChild(a);
+          }, 500);
+
+          function addImg(url) {
+              const image = document.createElement('img');
+              image.src = url;
+              image.style.width = "100%";
+              image.style.maxWidth = "800px";
+              out.appendChild(image);
+          }
+          setTimeout(function() {
+              out.appendChild(document.createElement('br'));
+              out.appendChild(document.createElement('br'));
+              out.appendChild(document.createTextNode("Alternatively, here are sample results in Firefox:"));
+              out.appendChild(document.createElement('br'));
+              out.appendChild(document.createElement('br'));
+              addImg("https://i.imgur.com/rIjkz6D.png");
+              out.appendChild(document.createElement('br'));
+              out.appendChild(document.createTextNode("and Chrome:"));
+              out.appendChild(document.createElement('br'));
+              out.appendChild(document.createElement('br'));
+              addImg("https://i.imgur.com/0VA6ioi.png");
+              out.appendChild(document.createElement('br'));
+          }, 1000);
+      }
+
+      function buildGraph(times) {
+          var margin = {
+                  top: 10,
+                  right: 40,
+                  bottom: 30,
+                  left: 40
+              },
+              width = Math.floor(document.body.clientWidth * 0.8) - margin.left - margin.right,
+              height = 400 - margin.top - margin.bottom;
+          d3.select("svg").remove();
+          let g = document.querySelector("#graph");
+          remove(g);
+          var res = Object.keys(times);
+          var color = d3.scaleOrdinal().domain(res).range(["#e41a1c", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00", "#ffff33", "#a65628", "#f781bf", "#999999"]);
+          let keys = Object.keys(times);
+          keys.sort();
+          for (let k of keys) {
+              let span = document.createElement("span");
+              span.style.color = color(k);
+              span.style.padding = "5px";
+              span.textContent = k;
+              g.appendChild(span);
+          }
+          g.appendChild(document.createElement("br"));
+          var svg = d3
+              .select("#graph")
+              .append("svg")
+              .attr("width", width + margin.left + margin.right)
+              .attr("height", height + margin.top + margin.bottom)
+              .append("g")
+              .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+          let d = [];
+          let all_data = [];
+          for (let k of res) {
+              d.push({
+                  key: k,
+                  values: times[k],
+              });
+              all_data = all_data.concat(times[k]);
+          }
+
+          var x = d3.scaleLog().base(2).domain(
+                  d3.extent(all_data, function(d) {
+                      return d[0];
+                  }))
+              .range([0, width]);
+          svg
+              .append("g")
+              .attr("transform", "translate(0," + height + ")")
+              .call(d3.axisBottom(x).ticks(25));
+
+          // text label for the x axis
+          svg
+              .append("text")
+              .attr("transform", "translate(" + width / 2 + " ," + (height + margin.top + 20) + ")")
+              .style("text-anchor", "middle")
+              .text("Vector Size");
+
+          var y = d3
+              .scaleLinear()
+              .domain([
+                  0,
+                  d3.max(all_data, function(d) {
+                      return d[1];
+                  }),
+              ])
+              .range([height, 0]);
+          svg.append("g").call(d3.axisLeft(y));
+
+          // text label for the y axis
+          svg
+              .append("text")
+              .attr("transform", "rotate(-90)")
+              .attr("y", 0 - margin.left)
+              .attr("x", 0 - height / 2)
+              .attr("dy", "1em")
+              .style("text-anchor", "middle")
+              .text("GB/s");
+
+          let lines = svg.selectAll(".line").data(d);
+
+          lines
+              .enter()
+              .append("path")
+              .attr("fill", "none")
+              .attr("stroke", function(d) {
+                  return color(d.key);
+              })
+              .attr("stroke-width", 1.5)
+              .attr("d", function(d) {
+                  return d3
+                      .line()
+                      .x(function(d) {
+                          return x(d[0]);
+                      })
+                      .y(function(d) {
+                          return y(d[1]);
+                      })(d.values);
+              })
+              .merge(lines)
+              .transition()
+              .duration(500);
+      }
+
+      function init(Module) {
+          Module._jit_add(4, 1);
+          let g = document.querySelector("#graph");
+          remove(g);
+          let button = document.createElement("button");
+          button.textContent = "Run Benchmarks";
+          button.addEventListener("click", function() {
+              let pre = document.querySelector("pre");
+              remove(pre);
+              let times = {};
+              let keys = ["Pure JS", "TypedArrays", "Emscripten (simd)", "Wasmblr", "Wasmblr (tuned)"];
+              for (let k of keys) {
+                  times[k] = [];
+              }
+              (async () => {
+                  for (let i = 2; i < 20; ++i) {
+                      const result = await benchmark(Module, Math.pow(2, i));
+                      for (let k of keys) {
+                          times[k].push([Math.pow(2, i), result[keys.indexOf(k)]]);
+                      }
+                      buildGraph(times);
+                  }
+              })();
+          });
+          document.getElementById("button_div").appendChild(button);
+      }
+
+      window.addEventListener("load", function() {
+          try {
+              createMyModule().then(init).catch(e => {
+                  setTimeout(error, 500);
+              });
+          } catch (e) {
+              setTimeout(error, 500);
+          };
+      });
